cb4f1246c477129cf59d68b96f221e08e9958fcd
dhamilton91 <dhamilton91@hotmail.com>
2017-09-01 13:28:59 +0100

    Update index.template.js

--- generators/reducerIndex/templates/index.template.js
+++ generators/reducerIndex/templates/index.template.js
@@ -1,9 +1,9 @@
-import {combineReducers} from 'redux';
+import { combineReducers } from 'redux';
 
-<% for (var i = 0; i < reducers.length; i++) { %>import <%= reducers[i].split('.')[0] %> from './containers/<%=reducers[i].split('.')[0]%>/reducer';
+<% for (var i = 0; i < reducers.length; i++) { %>import <%= reducers[i].split('.')[0][0].toLowerCase() + reducers[i].split('.')[0].slice(1) %> from './containers/<%=reducers[i].split('.')[0]%>/reducer';
 <% } %>
 
 export default combineReducers({
-<% for (var i = 0; i < reducers.length; i++) { %>    <%= reducers[i].split('.')[0] %>,
+<% for (var i = 0; i < reducers.length; i++) { %>    <%= reducers[i].split('.')[0][0].toLowerCase() + reducers[i].split('.')[0].slice(1) %>,
 <% } -%>
 });
